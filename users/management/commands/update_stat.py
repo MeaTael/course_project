cefe85ff2252731ed62d80315a9662d0eb1949c6
FPSarmin <fpsarmin@ya.ru>
2023-05-15 05:00:19 +0300

    fix bug with wrong learning and repeating words

--- users/management/commands/update_stat.py
+++ users/management/commands/update_stat.py
@@ -14,9 +14,9 @@ class Command(BaseCommand):
             words = LearnedWords.objects.all()
             for word in words:
                 timepassed = now - word.last_repeating
-                word.forgetting_coef = (1 + 2 ** word.repeating * profile.learning_level *
-                                        timepassed.total_seconds()/timedelta(hours=1)
-                                        .total_seconds()) ** (-1 / (2 ** word.repeating))
+                word.forgetting_coef = 1 - (1 - (1 + word.repeating * profile.learning_level *
+                                        timepassed.total_seconds()/timedelta(hours=1).total_seconds())
+                                            ** (-1 / word.repeating)) ** word.repeating
                 rating += word.forgetting_coef * 10
                 word.save()
             profile.rating = rating
